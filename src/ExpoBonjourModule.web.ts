02b0a01875e9d1822e98f09f1f8c412b41945c57
Aron <Random1996@163.com>
2025-10-28 16:31:17 +0800

    fix: fix web syntax error

--- src/ExpoBonjourModule.web.ts
+++ src/ExpoBonjourModule.web.ts
@@ -3,13 +3,7 @@ import { registerWebModule, NativeModule } from 'expo';
 import { ExpoBonjourModuleEvents } from './ExpoBonjour.types';
 
 class ExpoBonjourModule extends NativeModule<ExpoBonjourModuleEvents> {
-  PI = Math.PI;
-  async setValueAsync(value: string): Promise<void> {
-    this.emit('onChange', { value });
-  }
-  hello() {
-    return 'Hello world! 👋';
-  }
+  
 }
 
 export default registerWebModule(ExpoBonjourModule, 'ExpoBonjourModule');
